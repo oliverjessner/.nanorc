563dd6c74d423beb16a34113bffbc0f388d54105
Oliver Harald Jessner <oliverjessner@users.noreply.github.com>
2021-06-05 18:42:42 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Nano Editor (https://www.nano-editor.org) with syntax highlighting on the Mac
+# Version 4.0 ("Thy Rope of Sands") released March 24th 2019
+
+cd ~/
+wget https://www.nano-editor.org/dist/v4/nano-4.0.tar.gz
+tar -xvf nano-4.0.tar.gz
+mv nano-4.0 .nano
+cd .nano/
+./configure
+make
+sudo make install
